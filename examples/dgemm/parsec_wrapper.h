6d70c9fde9a7c5d20c81d66fe26fc612b322eaa5
Thomas Herault <herault@icl.utk.edu>
2016-10-12 10:25:46 -0400

    Initial PaRSEC / dgemm integration

--- examples/dgemm/parsec_wrapper.h
+++ examples/dgemm/parsec_wrapper.h
@@ -7,37 +7,9 @@
 #include <tiledarray.h>
 
 namespace Parsec {
-    class Handle {
-    protected:
-        dague_handle_t  *dague_handle_;
-        dague_context_t *dague_context_;
-        bool             handle_scheduled;
-
-    public:
-        Handle(dague_context_t *context) :
-            dague_context_(context),
-            dague_handle_(NULL),
-            handle_scheduled(false)
-        {
-        }
-
-        ~Handle() {
-            if( NULL != dague_handle_ ) {
-                /** For a reason, probably because it was not
-                 *  scheduled, the handle was not destructed by
-                 *  the specialized class. Try to memory collect
-                 *  using generic dague destructor */
-                OBJ_RELEASE(dague_handle_);
-            }
-            dague_handle_ = NULL;
-            dague_context_ = NULL;
-        }
-    };
-    
     class Parsec {
     private:
         dague_context_t *dague_context_;
-        int              nb_cores_;
         void initialize(std::vector<std::string> &v) {
             int argc = v.size();
             char **argv = new char*[argc+2];
@@ -47,7 +19,7 @@ namespace Parsec {
                 *p++ = strdup(s->c_str());
             }
             *p = NULL;
-            dague_context_ = dague_init(nb_cores_, &argc, &argv);
+            dague_context_ = dague_init(-1, &argc, &argv);
             for(int a = 0; a < argc; a++)
                 free(argv[a]);
             delete[] argv;
@@ -56,25 +28,10 @@ namespace Parsec {
     public:
         Parsec(void) {
             std::vector<std::string> empty_args;
-            nb_cores_ = -1;
-            initialize(empty_args);
-        }
-        Parsec(int nb_cores) {
-            std::vector<std::string> empty_args;
-            nb_cores_ = nb_cores;
             initialize(empty_args);
         }
         Parsec(std::vector<std::string> &v) {
-            nb_cores_ = -1;
             initialize(v);
-        }
-        Parsec(int nb_cores, std::vector<std::string> &v) {
-            nb_cores_ = nb_cores;
-            initialize(v);
-        }
-
-        dague_context_t *context(void) {
-            return dague_context_;
         }
         
         ~Parsec() {
