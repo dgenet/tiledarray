9e1db061428adf0c8c7378a3c5d8a9f69be27a9f
Thomas Herault <herault@icl.utk.edu>
2016-10-18 10:01:41 -0400

    Remove print debugs

--- examples/dgemm/irregular_tiled_matrix_wrapper.cpp
+++ examples/dgemm/irregular_tiled_matrix_wrapper.cpp
@@ -6,9 +6,7 @@ void *tilearray_future_get_tile(void *f)
     void *res;
     typedef TiledArray::Tensor<double, Eigen::aligned_allocator<double> > static_tile_type;
     typedef madness::Future<static_tile_type> static_future_type;
-    std::cout << "tilearray_future_get_tile: static casts done" << std::endl;
     static_future_type *F = reinterpret_cast <static_future_type*>(f);
-    std::cout << "tilearray_future_get_tile: reintrepret cast done" << std::endl;
     static_tile_type T = F->get();
     res = T.data();
     return res;
